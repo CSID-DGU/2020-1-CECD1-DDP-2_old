52fe78c1211c6fc6b1c26d438dc3e25cdd00af29
suna <jisuna314@gmail.com>
2020-06-19 19:51:14 +0900

    페이지 겹침 수정

--- WrapsodyChat/chatroom/src/index.js
+++ WrapsodyChat/chatroom/src/index.js
@@ -7,43 +7,19 @@ import ChatRoomApp from './components/UI_Components/ChatRoom/ChatRoomApp.js';
 import CreeatChatRoomApp from './components/UI_Components/CreateChatRoom/CreateChatRoom.js';
 import CreateChatRoom from './components/UI_Components/CreateChatRoom/CreateChatRoom.js';
 
-function Home(){
-    return(
-        <div>
-            <h2>Home</h2>
-            Home..
-        </div>
-    )
-}
-function Topics(){
-    return(
-        <div>
-            <h2>Topics</h2>
-            Topics..
-        </div>
-    )
-}
-function Contact(){
-    return(
-        <div>
-            <h2>Contact</h2>
-            Contact..
-        </div>
-    )
-}
 
 function App(){
     return(
         <div>
             <h1>DDP팀 데모 사이트</h1>
-            <ul>
-                <li><a href = "/">채팅방 생성</a></li>
-                <li><a href = "/topics">채팅방 리스트</a></li>
-                <li><a href = "/contact">채팅방</a></li>
+            <ul className = "demoUl">
+                <li className = "demoLi"><a href = "/createChatRoom"><img className = "demoImg" src = { require('./resources/images/speak.png')}/></a></li>
+                <li className = "demoLi"><a href = "/chatRoomList"><img className = "demoImg" src = { require('./resources/images/list.png')}/></a></li>
+                <li className = "demoLi"><a href = "/chatRoom"><img className = "demoImg" src = { require('./resources/images/chat.png')}/></a></li>
             </ul>
-            <Route exact path =""><CreateChatRoom></CreateChatRoom></Route>
-            <Route path = "/topics"><Topics></Topics></Route>
-            <Route path = "/contact"><ChatRoomApp></ChatRoomApp></Route>
+            <Route exact path ="/createChatRoom"><CreateChatRoom></CreateChatRoom></Route>
+            <Route path = "/chatRoomList"></Route>
+            <Route path = "/chatRoom"><ChatRoomApp></ChatRoomApp></Route>
         </div>
     )
    
